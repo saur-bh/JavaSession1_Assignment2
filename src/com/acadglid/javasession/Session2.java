46e5cb0a57dfbeae8c2fdace8407c3d42b31f01d
saurabhverma0609 <saurabh-verma@outlook.com>
2017-07-10 14:52:50 +0530

    Assignment 2

---
+++ src/com/acadglid/javasession/Session2.java
@@ -0,0 +1,29 @@
+/*
+ * Problem Statement :Write a program to print the sum of two numbers without using + operator. Other operators have to be used.
+ */
+
+package com.acadglid.javasession;
+
+public class Session2 {
+	
+	public static void main(String[] args) {
+
+	//1. Initialize the first number in CamelCase naming conventions.
+	
+		int firstNumber = 5;
+	
+	//2. Initialize the second number in CamelCase naming conventions.
+		
+		int secondNumber = 10;
+		
+	//3. subtract numbers 
+		
+		int sumBySubtract = firstNumber -(-secondNumber);
+		
+	//4. Print the result in the console output.
+		
+		System.out.println("The result for addition of two numbers is: "+sumBySubtract);
+	
+}
+
+}
